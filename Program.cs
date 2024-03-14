de548bafd29c7be6756d4050f38f6b20c52639a5
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2024-03-14 10:02:42 +0000

    Update

--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ GameManager gameManager = new GameManager(world);
 
 
 // make the monster
-Creature creature1 = new Creature("Orc", 100, 10, 10, 10, 10, null);
-Creature creature2 = new Creature("Orc", 100, 10, 10, 10, 10, null);
+Creature creature1 = new Creature("Orc nigga", 100, 10, 10, 10, 10, null);
+Creature creature2 = new Creature("Orc dude", 100, 10, 10, 10, 10, null);
 
 
 // add the monster to the world
