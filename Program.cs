b1c069f9c711a3a79493c4c5663be88ea8b9882f
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2024-03-14 09:08:26 +0000

    Update to the game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WorldOfPain.Models.Character;
+using WorldOfPain.Models.Environment;
+using WorldOfPain.Services;
+
+Console.WriteLine("Welcome to the game");
+
+
+// make the world
+World world = new World(25, 25);
+
+
+// make the game manager
+GameManager gameManager = new GameManager(world);
+
+
+// make the monster
+Creature creature1 = new Creature("Orc", 100, 10, 10, 10, 10, null);
+Creature creature2 = new Creature("Orc", 100, 10, 10, 10, 10, null);
+
+
+// add the monster to the world
+world.AddCreature(creature1);
+world.AddCreature(creature2);
+
+
+// run the game
+gameManager.StartGame();
